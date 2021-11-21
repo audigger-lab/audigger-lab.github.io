08cb7bb752c2fe7b956e5b4b42b2113e8ec081ab
audigger-lab <alexanderbv@gmx.de>
2021-11-21 13:08:49 -0800

    123

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ window.web3 = new Web3(window.ethereum)
 
   //Smart contract functions
   function registerSetInfo() {
+    console.log(account);
     info = $("#newInfo").val();
     contract.methods.putBet(info).send({from: account, value: info * 10 ** 18}).then( function(tx) { 
       console.log("Transaction: ", tx); 
@@ -108,5 +109,7 @@ window.web3 = new Web3(window.ethereum)
         console.log('Account: ' + account);
         web3.eth.defaultAccount = account;
       });
+
+      console.log(account);
   }
   
